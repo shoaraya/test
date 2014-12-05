15e2f7306fbe5a073f09c66812a1fc818ad9b66c
shoaraya <shoaraya@users.noreply.github.com>
2014-12-05 16:04:07 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 var milkcocoa = new MilkCocoa("https://io-si314xi3o.mlkcca.com");
 /* your-app-id にアプリ作成時に発行される"io-"から始まるapp-idを記入します */
 var chatDataStore = milkcocoa.dataStore("chat");
-var textArea, board;
+var textArea, board, textName;
 window.onload = function(){
   textArea = document.getElementById("msg");
   board = document.getElementById("board");
+  textName = document.getElementById("textName");
 }
 
 function clickEvent(){
   var text = textArea.value;
-  sendText(text);
+  sendText(text,textName,value);
 }
 
 function sendText(text){
